6a15216d00cd50ed03fb86513aaf50443915b9af
snaztoz <hafidhmn@gmail.com>
2024-09-08 13:10:25 +0700

    feat: :sparkles: add --create-passfile flag in "new" subcommand

--- cmd/gran/subcommand/new.go
+++ cmd/gran/subcommand/new.go
@@ -21,13 +21,14 @@ func (sc *New) Usage() string {
 
 func (sc *New) Handle(c *cli.Context) error {
 	sc.validate(c)
+	path := c.String("path")
 
 	passphrase, err := util.PromptPassphrase("Enter passphrase")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	f, err := os.Create(c.String("path"))
+	f, err := os.Create(path)
 	if err != nil {
 		return err
 	}
@@ -37,16 +38,42 @@ func (sc *New) Handle(c *cli.Context) error {
 		log.Fatal(err)
 	}
 
+	if createPassfile := c.Bool("create-passfile"); createPassfile {
+		sc.createPassfile(path+".gpass", passphrase)
+	}
+
 	return nil
 }
 
 func (sc *New) Flags() []cli.Flag {
-	return []cli.Flag{}
+	return []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "create-passfile",
+			Value: false,
+			Usage: "Create a new passphrase file as well",
+		},
+	}
 }
 
 func (sc *New) validate(c *cli.Context) {
 	path := c.String("path")
 	if util.IsFileExists(path) {
 		log.Fatal("file already exists: ", path)
+	}
+}
+
+func (sc *New) createPassfile(path, passphrase string) {
+	if util.IsFileExists(path) {
+		log.Fatal("file already exists: ", path)
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(passphrase); err != nil {
+		log.Fatal(err)
 	}
 }
